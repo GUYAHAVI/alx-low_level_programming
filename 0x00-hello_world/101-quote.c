c9d6f0c36fa210c505c0f2f505719981bf46a95d
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-17 01:49:40 -0700

    prints out results using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - start of code
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
