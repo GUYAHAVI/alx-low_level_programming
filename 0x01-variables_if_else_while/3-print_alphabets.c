73886606dd5020f836d45c58607aa25433b40a1f
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-17 23:58:17 -0700

    print alphabets in upper and lowercase using for loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet in upper and lowercase
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A' ; x <= 'Z' ; x++)
+	{
+		putchar(tolower(x));
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
