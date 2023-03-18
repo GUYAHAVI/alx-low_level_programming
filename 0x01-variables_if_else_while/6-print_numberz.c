163d2cbc4669e32b3f3fe6afde268b659598cfa6
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-18 01:22:19 -0700

    prints all single digit numbers of base 10 followed by a newline

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - prints single digit numbers of base 10 followed by a new line
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	do {
+		putchar(x);
+		x++;
+		putchar('\n');
+	}
+	while (x < 10)
+	return (0);
+}
