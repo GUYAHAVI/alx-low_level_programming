8ec0e1169d2c29eec32b23643c9ee066db332fde
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-21 03:51:51 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+*
+*prints _putchar
+*Return:always 0 (success)
+*
+*/
+
+int main(void)
+{
+	char j[] = "_putchar";
+
+	puts(j);
+	printf("\n");
+}
+
+
