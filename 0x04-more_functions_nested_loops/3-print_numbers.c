dcd1778968967674799de43e3f982bf09387c8a6
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-23 09:25:10 -0700

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-*print_numbers- prints numbers 0-9
-*
-*@i:number
-*Return:no return
-*/
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48 ; i <= 57 ; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
 }
-
