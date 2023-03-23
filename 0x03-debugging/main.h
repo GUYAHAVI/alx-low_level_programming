f3789b70fa62e2763704b5c31333adb28d513e9f
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-23 10:20:34 -0700

    contains all function definitions for this directory

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-int print_remaining_days();
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
