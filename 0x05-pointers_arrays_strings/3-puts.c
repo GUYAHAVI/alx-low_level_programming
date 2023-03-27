28fe268e6089cc906df8760fffd0dfac29686429
GUYAHAVI <harveyelvis24#gmail.com>
2023-03-27 12:17:31 -0700

    printing a string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-*_puts- prints a string to stdout
-*
-*@str: string to be printed
-*
+* _puts - Prints a string to stdout.
+* @str: The string to be printed.
 */
-void _puts(char *str);
+void _puts(char *str)
 {
-	_putchar(str);
+	while (*str)
+		_putchar(*str++);
+
 	_putchar('\n');
 }
+
