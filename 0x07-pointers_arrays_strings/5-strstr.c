97df7fdf12472636c3886f9f3ef92dc4270729d0
GUYAHAVI <harveyelvis24#gmail.com>
2023-04-04 10:59:27 -0700

    looks for needle in a haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strstr- function finds the first occurrence
+*of the substring needle in the string haystack
+*
+*@needle: string to be found
+*@haystack: string having the needle
+*
+*Return:Returns a pointer to the
+*beginning of the located substring,
+*or NULL if the substring is not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int j, k;
+
+	for (j = 0 ; haystack[j] != '\0'; j++)
+	{
+		for (k = 0 ; needle[k] != '\0' ; k++)
+		{
+			if (haystack[j] == needle[k])
+				return (haystack);
+		}
+	}
+	return ('\0');
+}
+
