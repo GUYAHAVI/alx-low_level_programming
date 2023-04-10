a77a99d8d14c7122ead21d79ef3f6369c7ef7cb4
GUYAHAVI <harveyelvis24#gmail.com>
2023-04-10 07:59:01 -0700

    static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strlen- prints the length of a string
+*
+*@s: string whose length is needed
+*
+*Return: length of a string
+*
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main- prints a string
+*
+*Return: always 0 on success
+*/
+int main(void)
+{
+	 _puts("\"At the end of the day,");
+	_puts(" my goal was to be the best");
+	_puts(" hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
